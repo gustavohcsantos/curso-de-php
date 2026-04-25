07238e34de6f8c6d7d212430084b1e2f2a7dda36
Gustavo <gustavohcsantos.dev@gmail.com>
2026-04-25 04:04:49 -0300

    Início dos tipos básicos para posteriores operações aritiméticas.

---
+++ tipos/int.php
@@ -0,0 +1,17 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+
+echo 11;
+echo '<br>';
+
+var_dump(11);
+echo '<br>';
+
+echo PHP_INT_MAX;
+echo '<br>';
+echo PHP_INT_MIN;
+echo '<br>';
+echo 017, '<br>'; // Base Octal
+echo 0b11, '<br>'; // Base Binária
+echo 0x11, '<br>'; // Base Hexadecimal
