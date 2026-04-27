5dad2010d50b6c0b0499d6edba73e07f86e00837
Gustavo <gustavohcsantos.dev@gmail.com>
2026-04-26 23:19:53 -0300

    Desafio string solucionado e ingressando ao tipo Booleano.

---
+++ tipos/booleano.php
@@ -0,0 +1,4 @@
+<div class="titulo">Tipo Booleano</div>
+
+<?php
+
--- tipos/desafio_string.php
+++ tipos/desafio_string.php
@@ -2,3 +2,13 @@
 
 <?php
 
+// Enunciado:
+// Avaliando os métodos da documentação da string, qual
+// é o método que pegará a posição do texto 'abc' na
+// string '!AbcaBcabc' retorna 1?
+
+$string = '!AbcaBcabc';
+$findme = 'abc';
+$position = stripos($string, $findme);
+
+echo "A string 'abc' foi encontrada em $string e existe na posição $position. ";
